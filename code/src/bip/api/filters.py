863554adf9f092562f3a7836fbe5da622fb0f106
Joeri Bekker <joeri@maykinmedia.nl>
2018-09-27 16:28:04 +0200

    Added wildcard filter for geslachtsnaam and added geboortedatum filter.

--- code/src/bip/api/filters.py
+++ code/src/bip/api/filters.py
@@ -5,11 +5,21 @@ from bip.datamodel.models import IngeschrevenNatuurlijkPersoon
 
 
 class IngeschrevenNatuurlijkPersoonFilter(FilterSet):
-    # postcode = django_filters.CharFilter(field_name='verblijfsplaats__postcode')
+    naam__geslachtsnaam = django_filters.CharFilter(
+        method='wildcard_filter', help_text='Zoeken op geslachtsnaam met ondersteuning voor wildcards (*).'
+    )
 
     class Meta:
         model = IngeschrevenNatuurlijkPersoon
         fields = (
             'burgerservicenummer',
-            # 'postcode'
+            'geboorte__datum',
+            'naam__geslachtsnaam',
         )
+
+    def wildcard_filter(self, queryset, name, value):
+        expression = r'^{}$'.format(value.replace('*', '.*'))
+
+        return queryset.filter(**{
+            f'{name}__iregex': expression
+        })
