cb2421e60c66975e8ce63e18b8cd057cf7a2614f
Melvin Lee <melvin.lee@iswish.nl>
2022-03-28 12:55:44 +0200

    feat(brpproxy): verblijfplaats mappings

--- src/BrpProxy/Mappers/VerblijfplaatsMapper.cs
+++ src/BrpProxy/Mappers/VerblijfplaatsMapper.cs
@@ -1,5 +1,7 @@
 ﻿using HaalCentraal.BrpProxy.Generated;
 using HaalCentraal.BrpProxy.Generated.Gba;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace BrpProxy.Mappers;
 
@@ -10,6 +12,77 @@ public static class VerblijfplaatsMapper
         return !string.IsNullOrWhiteSpace(verblijfplaats.Woonplaats)
             ? verblijfplaats.Woonplaats
             : verblijfplaats.GemeenteVanInschrijving?.Omschrijving;
+    }
+
+    public static string? MapStraat(this GbaVerblijfplaats verblijfplaats)
+    {
+        return !string.IsNullOrWhiteSpace(verblijfplaats.NaamOpenbareRuimte)
+            ? verblijfplaats.NaamOpenbareRuimte
+            : verblijfplaats.Straat;
+    }
+
+    public static string? MapAdresregel1(this GbaVerblijfplaats verblijfplaats)
+    {
+        StringBuilder retval = new();
+
+        if(!string.IsNullOrWhiteSpace(verblijfplaats.Straat))
+        {
+            retval.Append(verblijfplaats.Straat);
+        }
+        if (verblijfplaats.AanduidingBijHuisnummer != null)
+        {
+            switch (verblijfplaats.AanduidingBijHuisnummer.Code)
+            {
+                case "to":
+                    retval.Append(" t/o");
+                    break;
+                case "by":
+                    retval.Append(" bij");
+                    break;
+                default:
+                    break;
+            }
+        }
+        if(verblijfplaats.Huisnummer > 0)
+        {
+            retval.AppendFormat("{1}{0}", verblijfplaats.Huisnummer,
+                retval.Length > 0
+                    ? " "
+                    : "");
+        }
+        if (!string.IsNullOrWhiteSpace(verblijfplaats.Huisletter))
+        {
+            retval.AppendFormat(" {0}", verblijfplaats.Huisletter);
+        }
+        if (!string.IsNullOrWhiteSpace(verblijfplaats.Huisnummertoevoeging))
+        {
+            if (!string.IsNullOrWhiteSpace(verblijfplaats.Huisletter))
+            {
+                retval.Append(verblijfplaats.Huisnummertoevoeging);
+            }
+            else
+            {
+                retval.AppendFormat("{1}{0}", verblijfplaats.Huisnummertoevoeging,
+                    Regex.Match(verblijfplaats.Huisnummertoevoeging, @"^\d.*$").Success
+                        ? "-"
+                        : " ");
+            }
+        }
+
+        return retval.ToString();
+    }
+
+    public static string? MapAdresregel2(this GbaVerblijfplaats verblijfplaats)
+    {
+        var postcodeNum = verblijfplaats.Postcode[0..4];
+        var postcodeAlfa = verblijfplaats.Postcode[4..];
+
+        return $"{postcodeNum} {postcodeAlfa}  {verblijfplaats.MapWoonplaats()?.ToUpperInvariant()}";
+    }
+
+    public static string? MapLocatieAdresregel2(this GbaVerblijfplaats verblijfplaats)
+    {
+        return verblijfplaats.MapWoonplaats()?.ToUpperInvariant();
     }
 
     public static AbstractVerblijfplaatsBeperkt? Map(this GbaVerblijfplaatsBeperkt verblijfplaats)
--- src/BrpProxy/Profiles/VerblijfplaatsProfile.cs
+++ src/BrpProxy/Profiles/VerblijfplaatsProfile.cs
@@ -16,16 +16,26 @@ public class VerblijfplaatsProfile : Profile
             .ForMember(dest => dest.VerblijfplaatsOnbekend1, opt => opt.MapFrom(src => true));
         CreateMap<GbaVerblijfplaats, VerblijfplaatsBuitenland>()
             .ForMember(dest => dest.DatumInschrijvingInGemeente, opt => opt.MapFrom(src => src.DatumInschrijvingInGemeente.Map()))
-            .ForMember(dest => dest.DatumVan, opt => opt.MapFrom(src => src.DatumAanvangAdresBuitenland.Map()));
+            .ForMember(dest => dest.DatumVan, opt => opt.MapFrom(src => src.DatumAanvangAdresBuitenland.Map()))
+            .ForMember(dest => dest.Adresregel1, opt => opt.MapFrom(src => src.Adresregel1))
+            .ForMember(dest => dest.Adresregel2, opt => opt.MapFrom(src => src.Adresregel2))
+            .ForMember(dest => dest.Adresregel3, opt => opt.MapFrom(src => src.Adresregel3))
+            ;
         CreateMap<GbaVerblijfplaats, Adres>()
             .ForMember(dest => dest.DatumInschrijvingInGemeente, opt => opt.MapFrom(src => src.DatumInschrijvingInGemeente.Map()))
             .ForMember(dest => dest.DatumVan, opt => opt.MapFrom(src => src.DatumAanvangAdreshouding.Map()))
-            .ForMember(dest => dest.Woonplaats, opt => opt.MapFrom(src => src.MapWoonplaats()));
+            .ForMember(dest => dest.Woonplaats, opt => opt.MapFrom(src => src.MapWoonplaats()))
+            .ForMember(dest => dest.KorteNaam, opt => opt.MapFrom(src => src.Straat))
+            .ForMember(dest => dest.Straat, opt => opt.MapFrom(src => src.MapStraat()))
+            .ForMember(dest => dest.Adresregel1, opt => opt.MapFrom(src => src.MapAdresregel1()))
+            .ForMember(dest => dest.Adresregel2, opt => opt.MapFrom(src => src.MapAdresregel2()));
         CreateMap<GbaVerblijfplaats, Locatie>()
             .ForMember(dest => dest.DatumInschrijvingInGemeente, opt => opt.MapFrom(src => src.DatumInschrijvingInGemeente.Map()))
             .ForMember(dest => dest.Woonplaats, opt => opt.MapFrom(src => src.MapWoonplaats()))
-            //.ForMember(dest => dest.DatumVan, opt => opt.MapFrom(src => src.DatumAanvangAdreshouding.Map()))
-            ;
+            .ForMember(dest => dest.Adresregel1, opt => opt.MapFrom(src => src.Locatiebeschrijving))
+            .ForMember(dest => dest.Adresregel2, opt => opt.MapFrom(src => src.MapLocatieAdresregel2()));
+        //.ForMember(dest => dest.DatumVan, opt => opt.MapFrom(src => src.DatumAanvangAdreshouding.Map()))
+        ;
 
         CreateMap<GbaVerblijfplaatsBeperkt, AbstractVerblijfplaatsBeperkt>().ConvertUsing<VerblijfplaatsBeperktConverter>();
     }
