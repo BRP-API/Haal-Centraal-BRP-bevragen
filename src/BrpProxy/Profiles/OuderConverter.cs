f4a4950c4f259458520a2584e7e084c9c3daed52
Melvin Lee <melvin.lee@iswish.nl>
2022-04-11 12:23:01 +0200

    feat(brpproxy): start ouder mapping

---
+++ src/BrpProxy/Profiles/OuderConverter.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using HaalCentraal.BrpProxy.Generated;
+using HaalCentraal.BrpProxy.Generated.Gba;
+
+namespace BrpProxy.Profiles
+{
+    public class OuderConverter : ITypeConverter<GbaOuder, AbstractOuder>
+    {
+        public AbstractOuder Convert(GbaOuder source, AbstractOuder destination, ResolutionContext context)
+        {
+            return source switch
+            {
+                { Naam.Geslachtsnaam: var geslachtsnaam } when geslachtsnaam == "." => context.Mapper.Map<OnbekendOuder>(source),
+
+                _ => context.Mapper.Map<Ouder>(source)
+            };
+        }
+    }
+}
---
+++ src/BrpProxy/Profiles/OuderProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using BrpProxy.Mappers;
+using HaalCentraal.BrpProxy.Generated;
+using HaalCentraal.BrpProxy.Generated.Gba;
+
+namespace BrpProxy.Profiles;
+
+public class OuderProfile : Profile
+{
+    public OuderProfile()
+    {
+        CreateMap<GbaOuder, AbstractOuder>()
+            .ConvertUsing<OuderConverter>();
+        CreateMap<GbaOuder, OnbekendOuder>();
+        CreateMap<GbaOuder, Ouder>()
+            .ForMember(dest => dest.DatumIngangFamilierechtelijkeBetrekking, opt => opt.MapFrom(src => src.DatumIngangFamilierechtelijkeBetrekking.Map()));
+    }
+}
