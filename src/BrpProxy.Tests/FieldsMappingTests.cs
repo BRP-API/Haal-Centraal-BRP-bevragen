f946ab5318bab54f9040fe1143931c70c357fd2d
Melvin Lee <melvin.lee@iswish.nl>
2022-04-21 13:41:47 +0200

    tests(brpproxy): valideer field mappings

---
+++ src/BrpProxy.Tests/FieldsMappingTests.cs
@@ -0,0 +1,38 @@
+﻿using BrpProxy.Validators;
+using FluentAssertions;
+using HaalCentraal.BrpProxy.Generated;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace BrpProxy.Tests
+{
+    public class FieldsMappingTests
+    {
+        [Fact]
+        public void ValidatePersoonMapping()
+        {
+            var persoonFields = typeof(Persoon).GetPropertyPaths("HaalCentraal");
+
+            foreach (var line in File.ReadAllLines("../../../../../features/fields-mapping-Persoon.csv").Skip(1))
+            {
+                var field = line[(line.IndexOf(',')+1)..];
+
+                persoonFields.Contains(field).Should().BeTrue(field);
+            }
+        }
+
+        [Fact]
+        public void ValidatePersoonBeperktMapping()
+        {
+            var persoonBeperktFields = typeof(PersoonBeperkt).GetPropertyPaths("HaalCentraal");
+
+            foreach (var line in File.ReadAllLines("../../../../../features/fields-mapping-PersoonBeperkt.csv").Skip(1))
+            {
+                var field = line[(line.IndexOf(',') + 1)..];
+
+                persoonBeperktFields.Contains(field).Should().BeTrue(field);
+            }
+        }
+    }
+}
