33ae760b3f1673a890ddd0a4d9899e13d47618fd
Melvin Lee <melvin.lee@iswish.nl>
2022-05-18 10:53:37 +0200

    fix(brpproxy): geen serialisatie van lege verblijfadres

---
+++ src/BrpProxy/Generated/LocatiePartial.cs
@@ -0,0 +1,14 @@
+﻿namespace HaalCentraal.BrpProxy.Generated;
+
+public partial class Locatie
+{
+    public bool ShouldSerializeVerblijfadres() =>
+        Verblijfadres != null && Verblijfadres.ShouldSerialize();
+}
+
+public partial class VerblijfadresLocatie
+{
+    public bool ShouldSerialize() =>
+        !string.IsNullOrWhiteSpace(Locatiebeschrijving) ||
+        InOnderzoek != null;
+}
