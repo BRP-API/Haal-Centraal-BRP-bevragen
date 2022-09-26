27acbac929d3710bf27e9395376147a6dad5b494
fsamwel <37143495+fsamwel@users.noreply.github.com>
2022-09-26 12:04:54 +0200

    correctie mapping voor rni

--- features/step_definitions/step_defs.js
+++ features/step_definitions/step_defs.js
@@ -17,8 +17,6 @@ const propertyNameMap = new Map([
     ['geslachtsaanduiding (04.10)', 'geslacht.code'],
     ['indicatie geheim (70.10)', 'geheimhoudingPersoonsgegevens'],
     ['datum eerste inschrijving GBA (68.10)', 'datumEersteInschrijvingGBA'],
-    ['RNI-deelnemer (88.10)', 'rni_deelnemer'],
-    ['Omschrijving verdrag (88.20)', 'verdrag_oms'],
 
     // Geboorte
     ['geboortedatum (03.10)', 'datum'],
@@ -175,7 +173,10 @@ const columnNameMap = new Map([
     ['datum einde onderzoek (83.30)', 'onderzoek_eind_datum' ],
 	
     ['indicatie onjuist (84.10)', 'onjuist_ind' ],
-    ['datum ingang familierechtelijke betrekking (62.10)', 'familie_betrek_start_datum']
+    ['datum ingang familierechtelijke betrekking (62.10)', 'familie_betrek_start_datum'],
+
+    ['RNI-deelnemer (88.10)', 'rni_deelnemer'],
+    ['Omschrijving verdrag (88.20)', 'verdrag_oms']
 ]);
 
 Before(function() {
