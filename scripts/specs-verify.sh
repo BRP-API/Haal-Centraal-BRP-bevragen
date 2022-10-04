8abd7b8fdd9cb6505bf8aa015d53479bff4e6e31
Melvin Lee <melvin.lee@iswish.nl>
2022-10-04 08:39:28 +0200

    chore(specs): rapportage voor: - nationaliteit - gba nationaliteit scenario's - gba verblijfstitel scenario's - dev kind scenario's

--- scripts/specs-verify.sh
+++ scripts/specs-verify.sh
@@ -4,10 +4,11 @@ mkdir -p docs/features
 
 cucumber-js -f html:docs/features/test-result.html \
             -f json:docs/features/test-result.json \
-            gba-dev/kinderen-gba.feature \
-            gba-dev/verblijfstitel.feature \
+            features/dev/kind-velden.feature \
+            features/gba-dev/*.feature \
             features/aanhef.feature \
             features/aanschrijfwijze/**/*.feature \
             features/kinderen.feature \
+            features/nationaliteit.feature \
             features/verblijfstitel.feature \
             --tags "not @gba" --tags "not @skip-verify"
