d9331a7a8af20286c55a749cf51f453f3280ce6b
Melvin Lee <melvin.lee@iswish.nl>
2022-10-05 16:07:50 +0200

    chore(specs): rapportage voor: - dev europees kiesrecht - dev gezagsverhouding - dev opschorting bijhouding - dev ouder - dev partner - dev persoon adressering in onderzoek - dev uitsluiting kiesrecht

--- scripts/specs-verify.sh
+++ scripts/specs-verify.sh
@@ -4,8 +4,15 @@ mkdir -p docs/features
 
 cucumber-js -f html:docs/features/test-result.html \
             -f json:docs/features/test-result.json \
+            features/dev/europees-kiesrecht.feature \
+            features/dev/gezagsverhouding.feature \
             features/dev/kind-velden.feature \
             features/dev/nationaliteit*.feature \
+            features/dev/opschorting-bijhouding.feature \
+            features/dev/ouder.feature \
+            features/dev/partner.feature \
+            features/dev/persoon-adressering-inonderzoek.feature \
+            features/dev/uitsluiting-kiesrecht.feature \
             features/gba-dev/*.feature \
             features/aanhef.feature \
             features/aanschrijfwijze/**/*.feature \
