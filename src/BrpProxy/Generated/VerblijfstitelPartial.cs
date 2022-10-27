e52a681d371fb2d25ceb0adbbf28eb99b14f334f
Melvin Lee <melvin.lee@iswish.nl>
2022-10-27 08:14:44 +0200

    fix(brpproxy): verblijfstitel in onderzoek

--- src/BrpProxy/Generated/VerblijfstitelPartial.cs
+++ src/BrpProxy/Generated/VerblijfstitelPartial.cs
@@ -7,4 +7,14 @@ public partial class Verblijfstitel
         DatumEinde != null ||
         DatumIngang != null ||
         InOnderzoek != null;
+
+    public bool ShouldSerializeInOnderzoek() => InOnderzoek != null && InOnderzoek.ShouldSerialize();
+}
+
+public partial class VerblijfstitelInOnderzoek
+{
+    public bool ShouldSerialize() =>
+        Aanduiding.HasValue ||
+        DatumEinde.HasValue ||
+        DatumIngang.HasValue;
 }
