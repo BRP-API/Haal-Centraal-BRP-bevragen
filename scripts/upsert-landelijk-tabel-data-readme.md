7f508f976b41f27e3d6e8910d21add8d6f69e6dd
Melvin Lee <melvin.lee@iswish.nl>
2022-11-03 11:18:25 +0100

    fix(upsert-landelijk-tabel-data): documentatie +  nieuwe landelijk tabel csv bestanden

--- scripts/upsert-landelijk-tabel-data-readme.md
+++ scripts/upsert-landelijk-tabel-data-readme.md
@@ -10,6 +10,15 @@
 
 ## Landelijke tabellen die nu worden ge-update
 
+- Tabel37 Reden opnemen beëindigen nationaliteit
 - Tabel38 Adellijke titel predicaat
 - Tabel60 RNI-deelnemers
 - Tabel61 Gezagsverhouding
+
+## Opnemen van nieuwe/gewijzigde tabel waarden in de update script
+
+- Download de gewenste [landelijke tabel in csv-formaat](https://publicaties.rvig.nl/Landelijke_tabellen/Landelijke_tabellen_32_t_m_61_excl_tabel_35/Landelijke_Tabellen_32_t_m_61_in_csv_formaat) en bewaar het in de `scripts` map
+- Controleer in het `upsert-landelijk-tabel-data.js` bestand in de `scripts` map of er voor het gedownloade landelijk tabel bestand het volgende is opgenomen
+  - een _'bestand naam' naar 'tabel naam'_ mapping in de **tableNameMap**
+  - voor elke kolom in het bestand een _bestand kolom naam naar tabel kolom naam_ mapping in de **columnNameMap**
+  - een _upsertLandelijkTabel('\<naam tabel waarden bestand\>');_ statement in de **main()** functie
--- scripts/upsert-landelijk-tabel-data.js
+++ scripts/upsert-landelijk-tabel-data.js
@@ -11,19 +11,28 @@ const pool = new Pool({
   max: 150
 });
 
+// De lijst met tabelwaarden bestandsnamen zonder extensie met bijbehorende database tabel naam 
 const tableNameMap = new Map([
 
+  ['Tabel32 Nationaliteitentabel (gesorteerd op code)', 'lo3_nationaliteit'],
+  ['Tabel33 Gemeententabel (gesorteerd op code)', 'lo3_gemeente'],
+  ['Tabel34 Landentabel (gesorteerd op code)', 'lo3_land'],
+  ['Tabel37 Reden opnemen beëindigen nationaliteit', 'lo3_nl_nat_verkrijg_verlies_reden'],
   ['Tabel38 Adellijke titel predicaat','lo3_titel_predikaat'],
-  ['Tabel60 RNI-deelnemerstabel', 'lo3_rni_deelnemer' ],
-  ['Tabel61 Gezagsverhoudingtabel', 'lo3_gezagsverhouding'],
+  ['Tabel60 RNI-deelnemerstabel (gesorteerd op code)', 'lo3_rni_deelnemer' ],
+  ['Tabel61 Gezagsverhoudingtabel (gesorteerd op code)', 'lo3_gezagsverhouding'],
 
 ]);
 
+// De lijst met kolom namen uit de tabelwaarden bestanden met bijbehorende kolom naam in de database tabel
 const columnNameMap = new Map([
 
   ['02.21 Adellijke titel/predicaat', 'titel_predikaat'],
   ['02.02 Omschrijving', 'titel_predikaat_oms'],
   ['02.23 Soort', 'titel_predikaat_soort'],
+
+  ['05.11 Nationaliteitscode', 'nationaliteit_code'],
+  ['05.12 Omschrijving', 'nationaliteit_oms'],
 
   ['32.11 Gezagsverhouding', 'gezagsverhouding_code'],
   ['32.12 Omschrijving', 'gezagsverhouding_oms'],
@@ -31,10 +40,31 @@ const columnNameMap = new Map([
   ['88.11 Code RNI-deelnemer', 'deelnemer_code'],
   ['88.12 Omschrijving', 'deelnemer_oms'],
 
+  ['92.10 Gemeentecode', 'gemeente_code'],
+  ['92.11 Omschrijving', 'gemeente_naam'],
+  ['92.12 Nieuwe code', 'nieuwe_gemeente_code'],
+
+  ['94.10 Landcode', 'land_code'],
+  ['94.11 Omschrijving', 'land_naam'],
+
+  ['96.10 Reden opnemen/beëindigen nationaliteit', 'nl_nat_verkrijg_verlies_reden'],
+  ['96.20 Omschrijving', 'nl_nat_reden_oms'],
+  ['96.30 Soort', 'nl_nat_reden_soort'],
+
   ['99.98 Datum ingang', 'tabel_regel_start_datum'],
   ['99.99 Datum einde', 'tabel_regel_eind_datum'],
 
 ]);
+
+function main() {
+  upsertLandelijkTabel('Tabel32 Nationaliteitentabel (gesorteerd op code)');
+  upsertLandelijkTabel('Tabel33 Gemeententabel (gesorteerd op code)');
+  upsertLandelijkTabel('Tabel34 Landentabel (gesorteerd op code)');
+  upsertLandelijkTabel('Tabel37 Reden opnemen beëindigen nationaliteit');
+  upsertLandelijkTabel('Tabel38 Adellijke titel predicaat');
+  upsertLandelijkTabel('Tabel60 RNI-deelnemerstabel (gesorteerd op code)');
+  upsertLandelijkTabel('Tabel61 Gezagsverhoudingtabel (gesorteerd op code)');
+}
 
 function createUpsertStatement(tabelNaam, row) {
   const tableName = tableNameMap.get(tabelNaam);
@@ -49,13 +79,14 @@ function createUpsertStatement(tabelNaam, row) {
     const columnName = columnNameMap.get(key);
     const columnValue = row[key];
 
-    if(columnValue !== '') {
+    if(columnName !== undefined &&
+       (columnValue !== '' || columnName === 'nl_nat_reden_soort')) {
       columnNames.push(columnName);
 
       statement.text += index === 0
         ? `${columnName}`
         : `,${columnName}`;
-      statement.values.push(row[key]);
+      statement.values.push(columnValue);
     }
   });
 
@@ -78,28 +109,29 @@ function createUpsertStatement(tabelNaam, row) {
   return statement;
 }
 
-function upsertLandelijkTabel(filename) {
+async function upsertLandelijkTabel(filename) {
+  const statements = [];
+
   fs.createReadStream(`scripts/${filename}.csv`)
-  .pipe(parse({ delimiter: ';', columns: true}))
-  .on('data', async function(row) {
-    const client = await pool.connect();
-    try {
-      const statement = createUpsertStatement(filename, row);
-      console.log(statement);
+  .pipe(parse({ delimiter: ',', columns: true, bom: true}))
+  .on('data', function(row) {
+    const statement = createUpsertStatement(filename, row);
+    statements.push(statement);
+  });
+
+  const client = await pool.connect();
+  try {
+    for(const statement of statements) {
       await client.query(statement);
     }
-    catch(ex) {
-        console.log(ex.stack);
-    }
-    finally {
-        client.release();
-    }
-  })
-  .on('end', function() {
-    console.log('done');
-  });
+    console.log(`tabelwaarden uit '${filename}' geïmporteerd`);
+  }
+  catch(ex) {
+      console.log(ex.stack);
+  }
+  finally {
+      client.release();
+  }
 }
 
-upsertLandelijkTabel('Tabel38 Adellijke titel predicaat');
-upsertLandelijkTabel('Tabel60 RNI-deelnemerstabel');
-upsertLandelijkTabel('Tabel61 Gezagsverhoudingtabel');
+main();
