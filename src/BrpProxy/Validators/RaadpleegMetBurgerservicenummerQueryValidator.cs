fda46e4e87bb530019a55bc636657fafbc64a781
Melvin Lee <melvin.lee@iswish.nl>
2022-11-16 14:33:11 +0100

    feat(brpproxy): input validatie

--- src/BrpProxy/Validators/RaadpleegMetBurgerservicenummerQueryValidator.cs
+++ src/BrpProxy/Validators/RaadpleegMetBurgerservicenummerQueryValidator.cs
@@ -8,9 +8,20 @@ public class RaadpleegMetBurgerservicenummerQueryValidator : AbstractValidator<R
     const string RequiredErrorMessage = "required||Parameter is verplicht.";
     const string MinItemsErrorMessage = "minItems||Array bevat minder dan {0} items.";
     const string MaxItemsErrorMessage = "maxItems||Array bevat meer dan {0} items.";
+    const string BsnPattern = @"^[0-9]{9}$";
+    const string BsnPatternErrorMessage = $"pattern||Waarde voldoet niet aan patroon {BsnPattern}.";
 
     public RaadpleegMetBurgerservicenummerQueryValidator(FieldsHelper fieldsHelper)
     {
+        RuleFor(x => x.Burgerservicenummer)
+            .Cascade(CascadeMode.Stop)
+            .NotNull().WithMessage(RequiredErrorMessage)
+            .Must(x => x.Count > 0).WithMessage(string.Format(MinItemsErrorMessage, 1))
+            .Must(x => x.Count <= 20).WithMessage(string.Format(MaxItemsErrorMessage, 20));
+
+        RuleForEach(x => x.Burgerservicenummer)
+            .Matches(BsnPattern).WithMessage(BsnPatternErrorMessage);
+
         RuleFor(x => x.Fields)
             .Cascade(CascadeMode.Stop)
             .NotNull().WithMessage(RequiredErrorMessage)
