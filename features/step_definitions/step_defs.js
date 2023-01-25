09efe6a379a18ca58266184270cb3b092f3e988e
Melvin Lee <melvin.lee@iswish.nl>
2023-01-25 12:52:51 +0100

    fix(automation): zet default autorisatie

--- features/step_definitions/step_defs.js
+++ features/step_definitions/step_defs.js
@@ -287,7 +287,7 @@ async function deleteRecords(client, sqlData) {
 }
 
 async function deleteAdresRecord(client, sqlData) {
-    if(sqlData.ids === undefined) {
+    if(sqlData === undefined || sqlData.ids === undefined) {
         return;
     }
 
@@ -298,13 +298,28 @@ async function deleteAdresRecord(client, sqlData) {
     }
 }
 
+async function deleteAutorisatieRecords(client) {
+    const statement = {
+        text: `DELETE FROM public.${tableNameMap.get('autorisatie')} WHERE afnemer_code=$1`,
+        values: [8]
+    };
+
+    if(logSqlStatements) {
+        console.log(statement);
+    }
+
+    await client.query(statement);
+}
+
 function equals(sqlData, adresData) {
     return Object.keys(sqlData).length === adresData.length &&
            Object.keys(sqlData).every((v, i) => v === adresData[i])
 }
 
 After(async function() {
-    if(pool === undefined || !this.context.sql.cleanup) {
+    if (pool === undefined ||
+        !this.context.sql.cleanup ||
+        this.context.sqlData === undefined) {
         return;
     }
     
@@ -314,24 +329,21 @@ After(async function() {
 
         let adresData;
 
-        if(this.context.sqlData !== undefined) {
-            for(const sqlData of this.context.sqlData) {
-                if (equals(sqlData, ['adres', 'ids'])) {
-                    adresData = sqlData;
-                }
-                else {
-                    if (adresData !== undefined &&
-                        adresData.ids.adres_id == sqlData.ids.adres_id) {
-                            sqlData.ids.adres_id = undefined;
-                        }
-                    await deleteRecords(client, sqlData);
-                }
+        for(const sqlData of this.context.sqlData) {
+            if (equals(sqlData, ['adres', 'ids'])) {
+                adresData = sqlData;
+            }
+            else {
+                if (adresData !== undefined &&
+                    adresData.ids.adres_id == sqlData.ids.adres_id) {
+                        sqlData.ids.adres_id = undefined;
+                    }
+                await deleteRecords(client, sqlData);
             }
         }
 
-        if(adresData !== undefined) {
-            await deleteAdresRecord(client, adresData);
-        }
+        await deleteAdresRecord(client, adresData);
+        await deleteAutorisatieRecords(client);
     }
     catch(ex) {
         console.log(ex.stack);
@@ -1442,68 +1454,68 @@ async function postBevragenRequestWithBasicAuth(baseUrl, extraHeaders, dataTable
     }
 }
 
-When(/^personen wordt gezocht met de volgende parameters$/, async function (dataTable) {
-    await executeSqlStatements(this.context.sqlData);
-
-    addPersoonToPersonen(this.context);
-
-    const path = `${this.context.dataPath}/test-data.json`;
-    fs.writeFile(path, JSON.stringify(this.context.zoekResponse.personen, null, "\t"), (err) => {
-        if(err !== null) console.log(err);
-    });
-
-    if(this.context.oAuth.enable) {
-        const accessTokenUrl = this.context.oAuth.accessTokenUrl;
-        const oAuthSettings = this.context.oAuth.clients.find(client => client.afnemerID === this.context.afnemerId);
+async function handleOAuthRequest(oAuth, afnemerId, sqlDatas, endpointUrl, dataTable) {
+    const accessTokenUrl = oAuth.accessTokenUrl;
+    let oAuthSettings;
+    if(afnemerId === undefined) {
+        oAuthSettings = oAuth.clients[0];
+        console.log(`geen afnemer opgegeven voor scenario. oAuthSettings gebruiken van afnemer met ID '${oAuthSettings.afnemerID}'`);
+    }
+    else {
+        oAuthSettings = oAuth.clients.find(client => client.afnemerID === afnemerId);
         if(oAuthSettings === undefined) {
-            console.log(`geen oAuthSettings gevonden voor afnemerId '${this.context.afnemerId}'`);
-            return;
+            console.log(`geen oAuthSettings gevonden voor afnemerId '${afnemerId}'`);
+            return undefined;
         }
+    }
 
-        if(accessToken === undefined) {
-            console.log("no access token. authenticate");
-            accessToken = await getOAuthAccessToken(accessTokenUrl, oAuthSettings);
-        }
-        this.context.response = await postBevragenRequestWithOAuth(this.context.proxyUrl, accessToken, dataTable);
-        if(this.context.response.status === 401) {
-            console.log("access denied. access token expired");
-            accessToken = await getOAuthAccessToken(accessTokenUrl, oAuthSettings);
-            this.context.response = await postBevragenRequestWithOAuth(this.context.proxyUrl, accessToken, dataTable);
-        }
+    let sqlData = sqlDatas.at(-1);
+
+    sqlData['autorisatie'] = [
+        [
+            ['afnemer_code', oAuthSettings.afnemerID ],
+            ['geheimhouding_ind', 0],
+            ['verstrekkings_beperking', 0],
+            ['afnemer_naam', 'Haal Centraal'],
+            ['adres_vraag_bevoegdheid', 1],
+            ['ad_hoc_medium', 'N'],
+            ['tabel_regel_start_datum', 20220101],
+            ['ad_hoc_rubrieken', '10110 10120 10210 10220 10230 10240 10310 10320 10330 10410 16110 18110 18120 18210 18220 18230 18510 18610 20110 20120 20210 20220 20230 20240 20310 20320 20330 20410 26210 28110 28120 28210 28220 28230 28510 28610 30110 30120 30210 30220 30230 30240 30310 30320 30330 30410 36210 38110 38120 38210 38220 38230 38510 38610 40510 46310 46410 46510 48210 48220 48230 48510 48610 50110 50120 50210 50220 50230 50240 50310 50320 50330 50410 50610 50620 50630 50710 50720 50730 50740 51510 58110 58120 58210 58220 58230 58510 58610 60810 60820 60830 68110 68120 68210 68220 68230 68510 68610 76710 76720 76810 76910 77010 78710 80910 80920 81010 81020 81030 81110 81115 81120 81130 81140 81150 81160 81170 81180 81190 81210 81310 81320 81330 81340 81350 81410 81420 87210 87510 88510 88610 90110 90120 90210 90220 90230 90240 90310 90320 90330 98110 98120 98210 98220 98230 98510 98610 98910 103910 103920 103930 108510 108610 113210 113310 118210 118220 118230 118510 118610 123510 123520 123530 123540 123550 123560 123570 123610 128210 128220 128230 128510 128610 133110 133120 133130 133810 133820 138210 138220 138230 540510 546310 546410 546510 548210 548220 548230 548510 548610 550110 550120 550210 550220 550230 550240 550310 550320 550330 550410 550610 550620 550630 550710 550720 550730 550740 551510 558110 558120 558210 558220 558230 558510 558610 580910 580920 581010 581020 581030 581110 581115 581120 581130 581140 581150 581160 581170 581180 581190 581210 581310 581320 581330 581340 581350 581410 581420 587210 587510 588510 588610 603910 603920 603930 608510 608610']
+        ]
+    ];
+    await executeSqlStatements(sqlDatas);
+
+    if(accessToken === undefined) {
+        console.log("no access token. authenticate");
+        accessToken = await getOAuthAccessToken(accessTokenUrl, oAuthSettings);
+    }
+
+    let response = await postBevragenRequestWithOAuth(endpointUrl, accessToken, dataTable);
+    if(response.status === 401) {
+        console.log("access denied. access token expired");
+        accessToken = await getOAuthAccessToken(accessTokenUrl, oAuthSettings);
+        response = await postBevragenRequestWithOAuth(endpointUrl, accessToken, dataTable);
+    }
+
+    return response;
+}
+
+When(/^personen wordt gezocht met de volgende parameters$/, async function (dataTable) {
+    if(this.context.oAuth.enable) {
+        this.context.response = await handleOAuthRequest(this.context.oAuth, this.context.afnemerId, this.context.sqlData, this.context.proxyUrl, dataTable);
     } else {
+        await executeSqlStatements(this.context.sqlData);
+
         this.context.response = await postBevragenRequestWithBasicAuth(this.context.proxyUrl, this.context.extraHeaders, dataTable);
     }
 });
 
 When(/^gba personen wordt gezocht met de volgende parameters$/, async function (dataTable) {
-    await executeSqlStatements(this.context.sqlData);
-
-    addPersoonToPersonen(this.context);
-
-    const path = `${this.context.dataPath}/test-data.json`;
-    fs.writeFile(path, JSON.stringify(this.context.zoekResponse.personen, null, "\t"), (err) => {
-        if(err !== null) console.log(err);
-    });
-
     if(this.context.oAuth.enable) {
-        const accessTokenUrl = this.context.oAuth.accessTokenUrl;
-        const oAuthSettings = this.context.oAuth.clients.find(client => client.afnemerID === this.context.afnemerId);
-        if(oAuthSettings === undefined) {
-            console.log(`geen oAuthSettings gevonden voor afnemerId '${this.context.afnemerId}'`);
-            return;
-        }
-
-        if(accessToken === undefined) {
-            console.log("no access token. authenticate");
-            accessToken = await getOAuthAccessToken(accessTokenUrl, oAuthSettings);
-        }
-        this.context.response = await postBevragenRequestWithOAuth(this.context.gbaUrl, accessToken, dataTable);
-        if(this.context.response.status === 401) {
-            console.log("access denied. access token expired");
-            accessToken = await getOAuthAccessToken(accessTokenUrl, oAuthSettings);
-            this.context.response = await postBevragenRequestWithOAuth(this.context.gbaUrl, accessToken, dataTable);
-        }
+        this.context.response = await handleOAuthRequest(this.context.oAuth, this.context.afnemerId, this.context.sqlData, this.context.gbaUrl, dataTable);
     } else {
+        await executeSqlStatements(this.context.sqlData);
+
         this.context.response = await postBevragenRequestWithBasicAuth(this.context.gbaUrl, this.context.extraHeaders, dataTable);
     }
 });
