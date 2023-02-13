26c7105bb4691b38107e2c34cd24fac97bd8f753
Melvin Lee <melvin.lee@iswish.nl>
2023-02-13 16:24:55 +0100

    fix(brpproxy): parameter validatie

--- src/BrpProxy/Validators/PersoonFieldsValidator.cs
+++ src/BrpProxy/Validators/PersoonFieldsValidator.cs
@@ -5,7 +5,7 @@ namespace BrpProxy.Validators
     public class PersoonFieldsValidator : AbstractValidator<string>
     {
         const string RequiredErrorMessage = "required||Parameter is verplicht.";
-        const string FieldPattern = @"^[a-zA-Z0-9\._]{1,199}$";
+        const string FieldPattern = @"^[a-zA-Z0-9\._]{1,200}$";
         const string FieldPatternErrorMessage = $"pattern||Waarde voldoet niet aan patroon {FieldPattern}.";
         const string FieldExistErrorMessage = "fields||Parameter bevat een niet bestaande veldnaam.";
 
