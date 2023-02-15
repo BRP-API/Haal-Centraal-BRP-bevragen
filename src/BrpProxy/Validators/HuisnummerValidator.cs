24d8ccf155149e2e1acad139c3352985e3aba7aa
Melvin Lee <melvin.lee@iswish.nl>
2023-02-15 10:24:16 +0100

    fix(brpproxy): huisnummer validatie

---
+++ src/BrpProxy/Validators/HuisnummerValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+
+namespace BrpProxy.Validators
+{
+    public class HuisnummerValidator : AbstractValidator<string?>
+    {
+        const string RequiredErrorMessage = "required||Parameter is verplicht.";
+        const string NumberPattern = @"\d+";
+        const string NumberErrorMessage = "integer||Waarde is geen geldig getal.";
+        const string HuisnummerMinimumErrorMessage = "minimum||Waarde is lager dan minimum 1.";
+        const string HuisnummerMaximumErrorMessage = "maximum||Waarde is hoger dan maximum 99999.";
+
+        private int? StringToNullableInt(string? value) =>
+            int.TryParse(value, out var result)
+                ? result
+                : null;
+
+        public HuisnummerValidator()
+        {
+            RuleFor(x => x)
+                .NotEmpty().WithMessage(RequiredErrorMessage)
+                .Matches(NumberPattern).WithMessage(NumberErrorMessage);
+
+            Transform(x => x, StringToNullableInt)
+                .GreaterThan(0).WithMessage(HuisnummerMinimumErrorMessage);
+
+            Transform(x => x, StringToNullableInt)
+                .LessThan(100000).WithMessage(HuisnummerMaximumErrorMessage);
+        }
+    }
+}
--- src/BrpProxy/Validators/ZoekMetPostcodeEnHuisnummerQueryValidator.cs
+++ src/BrpProxy/Validators/ZoekMetPostcodeEnHuisnummerQueryValidator.cs
@@ -1,46 +1,43 @@
 ﻿using FluentValidation;
 using HaalCentraal.BrpProxy.Generated;
 
-namespace BrpProxy.Validators
+namespace BrpProxy.Validators;
+
+public class ZoekMetPostcodeEnHuisnummerQueryValidator : PersonenQueryValidator<ZoekMetPostcodeEnHuisnummer>
 {
-    public class ZoekMetPostcodeEnHuisnummerQueryValidator : PersonenQueryValidator<ZoekMetPostcodeEnHuisnummer>
+    const string RequiredErrorMessage = "required||Parameter is verplicht.";
+    const string HuisletterPattern = @"^[a-zA-Z]{1}$";
+    const string HuisletterErrorMessage = $"pattern||Waarde voldoet niet aan patroon {HuisletterPattern}.";
+    const string HuisnummertoevoegingPattern = @"^[a-zA-Z0-9 \-]{1,4}$";
+    const string HuisnummertoevoegingErrorMessage = $"pattern||Waarde voldoet niet aan patroon {HuisnummertoevoegingPattern}.";
+    const string PostcodePattern = @"^[1-9]{1}[0-9]{3}[ ]?[A-Za-z]{2}$";
+    const string PostcodeErrorMessage = $"pattern||Waarde voldoet niet aan patroon {PostcodePattern}.";
+
+    public ZoekMetPostcodeEnHuisnummerQueryValidator(FieldsHelper fieldsHelper)
+        : base(fieldsHelper)
     {
-        const string RequiredErrorMessage = "required||Parameter is verplicht.";
-        const string HuisletterPattern = @"^[a-zA-Z]{1}$";
-        const string HuisletterErrorMessage = $"pattern||Waarde voldoet niet aan patroon {HuisletterPattern}.";
-        const string HuisnummertoevoegingPattern = @"^[a-zA-Z0-9 \-]{1,4}$";
-        const string HuisnummertoevoegingErrorMessage = $"pattern||Waarde voldoet niet aan patroon {HuisnummertoevoegingPattern}.";
-        const string NumberPattern = @"^([1-9]|[1-9][0-9]|[1-9][0-9][0-9]|[1-9][0-9][0-9][0-9]|[1-9][0-9][0-9][0-9][0-9])$";
-        const string NumberErrorMessage = "integer||Waarde is geen geldig getal.";
-        const string PostcodePattern = @"^[1-9]{1}[0-9]{3}[ ]?[A-Za-z]{2}$";
-        const string PostcodeErrorMessage = $"pattern||Waarde voldoet niet aan patroon {PostcodePattern}.";
+        RuleForEach(x => x.AdditionalProperties)
+            .SetValidator(new AdditionalPropertiesValidator())
+            .When(x => x.AdditionalProperties.Count > 0);
 
-        public ZoekMetPostcodeEnHuisnummerQueryValidator(FieldsHelper fieldsHelper)
-            : base(fieldsHelper)
-        {
-            RuleForEach(x => x.AdditionalProperties)
-                .SetValidator(new AdditionalPropertiesValidator())
-                .When(x => x.AdditionalProperties.Count > 0);
+        RuleFor(x => x.Postcode)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty().WithMessage(RequiredErrorMessage)
+            .Matches(PostcodePattern).WithMessage(PostcodeErrorMessage);
 
-            RuleFor(x => x.Postcode)
-                .Cascade(CascadeMode.Stop)
-                .NotEmpty().WithMessage(RequiredErrorMessage)
-                .Matches(PostcodePattern).WithMessage(PostcodeErrorMessage);
+        RuleFor(x => x.Huisnummer)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty().WithMessage(RequiredErrorMessage)
+            .SetValidator(new HuisnummerValidator());
 
-            RuleFor(x => x.Huisnummer)
-                .Cascade(CascadeMode.Stop)
-                .NotEmpty().WithMessage(RequiredErrorMessage)
-                .Matches(NumberPattern).WithMessage(NumberErrorMessage);
+        RuleFor(x => x.Huisletter)
+            .Cascade(CascadeMode.Stop)
+            .Matches(HuisletterPattern).WithMessage(HuisletterErrorMessage)
+            .When(x => !string.IsNullOrWhiteSpace(x.Huisletter));
 
-            RuleFor(x => x.Huisletter)
-                .Cascade(CascadeMode.Stop)
-                .Matches(HuisletterPattern).WithMessage(HuisletterErrorMessage)
-                .When(x => !string.IsNullOrWhiteSpace(x.Huisletter));
-
-            RuleFor(x => x.Huisnummertoevoeging)
-                .Cascade(CascadeMode.Stop)
-                .Matches(HuisnummertoevoegingPattern).WithMessage(HuisnummertoevoegingErrorMessage)
-                .When(x => !string.IsNullOrWhiteSpace(x.Huisnummertoevoeging));
-        }
+        RuleFor(x => x.Huisnummertoevoeging)
+            .Cascade(CascadeMode.Stop)
+            .Matches(HuisnummertoevoegingPattern).WithMessage(HuisnummertoevoegingErrorMessage)
+            .When(x => !string.IsNullOrWhiteSpace(x.Huisnummertoevoeging));
     }
 }
