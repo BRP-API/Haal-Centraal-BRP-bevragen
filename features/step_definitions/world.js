a36eaedc25491fdb05ce4dae0893bc6bd727e18c
Melvin Lee <melvin.lee@iswish.nl>
2023-05-24 10:50:00 +0200

    chore(automation): enable certificate validation

--- features/step_definitions/world.js
+++ features/step_definitions/world.js
@@ -44,6 +44,6 @@ class World {
 }
 
 // set this environment variable to bypass DEPTH_ZERO_SELF_SIGNED_CERT error when using self-signed certificates
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 module.exports = {World}
