637a1a8f82eeafdfd1df75e6a28418aec0e6fd93
Melvin Lee <melvin.lee@iswish.nl>
2024-02-21 13:37:55 +0100

    feat(gezagmock): feature complete - alle endpoints geïmplementeerd - gezagsrelaties worden gehaald uit test-data.json bestand

---
+++ src/GezagMock/Repositories/GezagsrelatieRepository.cs
@@ -0,0 +1,30 @@
+﻿using Brp.Gezag.Mock.Generated;
+using GezagMock.Generated;
+using Newtonsoft.Json;
+
+namespace GezagMock.Repositories;
+
+public class GezagsrelatieRepository
+{
+    private readonly IWebHostEnvironment _environment;
+
+    public GezagsrelatieRepository(IWebHostEnvironment environment)
+    {
+        _environment = environment;
+    }
+
+    public async Task<ICollection<Gezagsrelatie>> Zoek(string bsn)
+    {
+        var path = Path.Combine(_environment.ContentRootPath, "Data", "test-data.json");
+        if(!File.Exists(path))
+        {
+            throw new FileNotFoundException($"invalid file: '{path}'");
+        }
+
+        var data = await File.ReadAllTextAsync(path);
+
+        var retval = JsonConvert.DeserializeObject<List<Gezag>>(data)?.Find(x => x.Burgerservicenummer == bsn);
+
+        return retval?.Gezagsrelaties ?? new List<Gezagsrelatie>();
+    }
+}
