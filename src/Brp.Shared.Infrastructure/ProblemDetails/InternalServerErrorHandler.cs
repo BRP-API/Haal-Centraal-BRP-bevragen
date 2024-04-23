66d5f564fc02e04bb3406e05b477be54c7688958
MelvLee <github-action@actions-template-sync.noreply.github.com>
2024-04-23 00:15:52 +0000

    chore: sync met 'BRP-API/brp-shared-dotnet' repo
    Signed-off-by: MelvLee <github-action@actions-template-sync.noreply.github.com>

--- src/Brp.Shared.Infrastructure/ProblemDetails/InternalServerErrorHandler.cs
+++ src/Brp.Shared.Infrastructure/ProblemDetails/InternalServerErrorHandler.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Brp.Shared.Infrastructure.Http;
+using Microsoft.AspNetCore.Http;
 
 namespace Brp.Shared.Infrastructure.ProblemDetails;
 
@@ -6,24 +7,8 @@ public static class InternalServerErrorHandler
 {
     public static async Task HandleInternalServerError(this HttpContext context)
     {
-        var problemDetails = context.Request.CreateInternalServerErrorProblemDetails();
+        var problemDetails = context.Request.CreateProblemDetailsFor(StatusCodes.Status500InternalServerError);
 
-        context.Response.StatusCode = problemDetails.Status!.Value;
-
-        await context.Response.WriteAsJsonAsync(
-            problemDetails,
-            System.Text.Json.JsonSerializerOptions.Default,
-            ProblemDetailsHelpers.ProblemDetailsContentType);
-    }
-
-    public static Microsoft.AspNetCore.Mvc.ProblemDetails CreateInternalServerErrorProblemDetails(this HttpRequest request)
-    {
-        return new Microsoft.AspNetCore.Mvc.ProblemDetails
-        {
-            Instance = request.Path.ToUriComponent(),
-            Status = StatusCodes.Status500InternalServerError,
-            Title = "Internal Server error.",
-            Type = StatusCodeIdentifiers.InternalServerErrorIdentifier
-        };
+        await context.Response.WriteProblemDetailsAsync(problemDetails);
     }
 }
