3d4e740f1e7e034ecbb8053f69f5d355dee41eed
MelvLee <github-action@actions-template-sync.noreply.github.com>
2024-05-22 00:15:58 +0000

    chore: sync met 'BRP-API/brp-shared-dotnet' repo
    Signed-off-by: MelvLee <github-action@actions-template-sync.noreply.github.com>

---
+++ src/Brp.Shared.Infrastructure/Validatie/Validators/ReisdocumentnummerValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using Newtonsoft.Json.Linq;
+
+namespace Brp.Shared.Infrastructure.Validatie.Validators;
+
+public class ReisdocumentnummerValidator : AbstractValidator<JObject>
+{
+    const string ParameterNaam = "reisdocumentnummer";
+
+    const string RequiredErrorMessage = "required||Parameter is verplicht.";
+    const string ReisdocumentnummerPattern = @"^[0-9]{9}$";
+    const string ReisdocumentnummerPatternErrorMessage = $"pattern||Waarde voldoet niet aan patroon {ReisdocumentnummerPattern}.";
+
+    public ReisdocumentnummerValidator(bool isVerplichtVeld = false)
+    {
+        if (isVerplichtVeld)
+        {
+            RuleFor(x => x.Value<string>(ParameterNaam))
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty().WithMessage(RequiredErrorMessage)
+                .Matches(ReisdocumentnummerPattern).WithMessage(ReisdocumentnummerPatternErrorMessage)
+                .OverridePropertyName(ParameterNaam)
+                ;
+        }
+        else
+        {
+            RuleFor(x => x.Value<string>(ParameterNaam))
+                .Cascade(CascadeMode.Stop)
+                .Matches(ReisdocumentnummerPattern).WithMessage(ReisdocumentnummerPatternErrorMessage)
+                .OverridePropertyName(ParameterNaam)
+                ;
+        }
+    }
+}
