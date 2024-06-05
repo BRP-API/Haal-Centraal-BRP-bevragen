9ce7f6ae0882824db6995160866ae0f3ead2bcf2
MelvLee <github-action@actions-template-sync.noreply.github.com>
2024-06-05 00:16:28 +0000

    chore: sync met 'BRP-API/brp-shared-dotnet' repo
    Signed-off-by: MelvLee <github-action@actions-template-sync.noreply.github.com>

--- src/Brp.Shared.Infrastructure/Autorisatie/IAuthorisation.cs
+++ src/Brp.Shared.Infrastructure/Autorisatie/IAuthorisation.cs
@@ -3,5 +3,4 @@
 public interface IAuthorisation
 {
     AuthorisationResult Authorize(int afnemerCode, int? gemeenteCode, string requestBody);
-    AuthorisationResult AuthorizeResponse(int afnemerCode, int? gemeenteCode, string? geleverdeGemeenteCodes);
 }
