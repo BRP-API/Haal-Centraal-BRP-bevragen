79b7fda3d997fca5dc7a74134c83bfa3f0c2cc07
MelvLee <github-action@actions-template-sync.noreply.github.com>
2024-06-27 00:16:46 +0000

    chore: sync met 'BRP-API/brp-shared-dotnet' repo
    Signed-off-by: MelvLee <github-action@actions-template-sync.noreply.github.com>

---
+++ src/Brp.Shared.Infrastructure/Utils/DateTimeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace Brp.Shared.Infrastructure.Utils;
+
+public static class DateTimeExtensions
+{
+    static readonly CultureInfo _nl = new("nl-NL");
+    const string _dateFormat = "yyyy-MM-dd";
+
+    public static DateTimeOffset ToDateTimeOffset(this string dateTime)
+    {
+        return DateTimeOffset.ParseExact(dateTime, _dateFormat, _nl.DateTimeFormat, DateTimeStyles.None);
+    }
+
+    public static bool IsDateTime(this string? dateTime)
+    {
+        return DateTime.TryParseExact(dateTime, _dateFormat, _nl.DateTimeFormat, DateTimeStyles.None, out _);
+    }
+}
