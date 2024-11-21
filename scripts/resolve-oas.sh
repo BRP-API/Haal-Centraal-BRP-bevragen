3588254f8dfdf232628b25cdc258233ca9f41e5d
Melvin Lee <melvin.lee@iswish.nl>
2024-11-21 08:28:30 +0100

    chore(specs): minderjarige en toelichting veld toegevoegd aan TijdelijkGeenGezag type

--- scripts/resolve-oas.sh
+++ scripts/resolve-oas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-redocly bundle ./specificatie/openapi.yaml -o ./specificatie/genereervariant/openapi.yaml
-redocly bundle ./specificatie/openapi.yaml -o ./specificatie/genereervariant/openapi.json
-redocly bundle ./specificatie/gba-openapi.yaml -o ./specificatie/gba-genereervariant/openapi.yaml
-redocly bundle ./specificatie/gba-openapi.yaml -o ./specificatie/gba-genereervariant/openapi.json
+npx redocly bundle ./specificatie/openapi.yaml -o ./specificatie/genereervariant/openapi.yaml
+npx redocly bundle ./specificatie/openapi.yaml -o ./specificatie/genereervariant/openapi.json
+npx redocly bundle ./specificatie/gba-openapi.yaml -o ./specificatie/gba-genereervariant/openapi.yaml
+npx redocly bundle ./specificatie/gba-openapi.yaml -o ./specificatie/gba-genereervariant/openapi.json
