e26288c1abb0e7bb1e3b5e1fffbf49cfb39cc4e0
Melvin Lee <melvin.lee@iswish.nl>
2024-12-02 16:03:08 +0100

    chore(workflow): lint-oas.yml workflow & oas bash scripts gecorrigeerd

--- scripts/oas-lint.sh
+++ scripts/oas-lint.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-npx spectral lint ./specificatie/referentie-data-api/openapi.yaml
-npx spectral lint ./specificatie/referentie-informatie-api/openapi.yaml
+npx spectral lint ./specificatie/openapi.yaml
--- scripts/oas-resolve.sh
+++ scripts/oas-resolve.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-npx redocly bundle ./specificatie/referentie-data-api/openapi.yaml -o ./specificatie/referentie-data-api/resolved/openapi.yaml
-npx redocly bundle ./specificatie/referentie-informatie-api/openapi.yaml -o ./specificatie/referentie-informatie-api/resolved/openapi.yaml
+npx redocly bundle ./specificatie/openapi.yaml -o ./specificatie/resolved/openapi.yaml
+npx redocly bundle ./specificatie/openapi.yaml -o ./specificatie/resolved/openapi.json
