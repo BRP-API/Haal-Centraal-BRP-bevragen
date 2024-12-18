f406a19ade41b520e73138a7f17d2f157736052d
MelvLee <github-action@actions-template-sync.noreply.github.com>
2024-12-18 13:58:01 +0000

    chore: sync met 'BRP-API/brp-shared' repo
    Signed-off-by: MelvLee <github-action@actions-template-sync.noreply.github.com>

--- scripts/specs-verify.sh
+++ scripts/specs-verify.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+echo "### Cucumber Test Reports"
+
+echo "#### Step definitions validatie"
+
 npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-zonder-dependency-integratie.json \
                 -f summary:./test-reports/cucumber-js/step-definitions/test-result-zonder-dependency-integratie-summary.txt \
                 -f summary \
                 features/docs \
                 --tags "not @integratie" \
-                --tags "not @skip-verify"
+                --tags "not @skip-verify" \
+                > /dev/null
 
+node ./scripts/generate-step-summary.js ./test-reports/cucumber-js/step-definitions/test-result-zonder-dependency-integratie-summary.txt ./test-reports/cucumber-js/step-summary.txt
+cat ./test-reports/cucumber-js/step-summary.txt
