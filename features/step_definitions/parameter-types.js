6c0e43eb6df4344ef1680def7a8abe4c9217eac1
MelvLee <github-action@actions-template-sync.noreply.github.com>
2025-05-13 00:23:07 +0000

    chore: sync met 'BRP-API/brp-shared' repo
    Signed-off-by: MelvLee <github-action@actions-template-sync.noreply.github.com>

--- features/step_definitions/parameter-types.js
+++ features/step_definitions/parameter-types.js
@@ -42,10 +42,15 @@ function defineDateParameterType(name, regexp, transformer) {
 
 defineParameterType({
     name: 'aanduidingen',
-    regexp: /'([a-zA-Z0-9, ]*)'/,
+    regexp: /'([a-zA-Z0-9À-ž-, ]*)'/,
     transformer(aanduidingen) {
         return aanduidingen.replace(' en ', ',').split(',').map(aanduiding => aanduiding.trim());
     }
+});
+
+defineParameterType({
+    name: 'aanduiding',
+    regexp: /'([a-zA-Z0-9À-ž-]*)'/
 });
 
 defineParameterType({
@@ -106,3 +111,12 @@ defineParameterType({
     name: 'relatieve datum',
     regexp: /(gisteren|vandaag|morgen|deze maand|vorige maand|volgende maand)/
 });
+
+defineParameterType({
+    name: 'tijdelijk geen gezag of niet te bepalen',
+    regexp: /(tijdelijk geen gezag|niet te bepalen)/,
+});
+
+defineParameterType({
+    name: 'toelichting',
+    regexp: /[a-zA-Z0-9À-ž /;:.\-?()]{1,400}/});
