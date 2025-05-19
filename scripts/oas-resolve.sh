c73f6a293a2a145c6701e9d04c0e896175304f43
Melvin Lee <melvin.lee@iswish.nl>
2025-05-19 15:41:48 +0200

    chore(oas): veilig stellen v2.6.0 spec en aanpassingen tbv bsn-loze ouders

--- scripts/oas-resolve.sh
+++ scripts/oas-resolve.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+npx redocly bundle ./specificatie/openapi-v1.yaml -o ./specificatie/resolved/openapi-v1.yaml
+npx redocly bundle ./specificatie/openapi-v1.yaml -o ./specificatie/resolved/openapi-v1.json
 npx redocly bundle ./specificatie/openapi.yaml -o ./specificatie/resolved/openapi.yaml
 npx redocly bundle ./specificatie/openapi.yaml -o ./specificatie/resolved/openapi.json
